04c5940cb82ec2ab222c20f48699496b4d5219af
night <835914840@qq.com>
2017-03-29 11:37:56 +0800

    封装ajax

---
+++ web/ajax.js
@@ -0,0 +1,24 @@
+/**
+ * Created by we on 2017/3/29.
+ */
+function ajax(url, onSuccess)
+{
+    var xmlhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft XMLHTTP');
+    xmlhttp.open("POST", url, true);
+    xmlhttp.send();
+
+    xmlhttp.onreadystatechange = function ()
+    {
+        if (xmlhttp.readyState == 4)
+        {
+            if (xmlhttp.status == 200)
+            {
+                onSuccess(xmlhttp.responseText);
+            }
+            else
+            {
+                alert("Ajax return Error");
+            }
+        }
+    }
+}
